7d8434a19e78087344a76b56d47030958a52655c
Elizabeth Flournoy <bflournoy@gmail.com>
2017-07-23 16:56:12 -0600

    updated flexbox arrangement, added powered by footer, update button styling to outline only

--- client/components/Splash.js
+++ client/components/Splash.js
@@ -17,13 +17,21 @@ export default class Start extends React.Component {
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
-        <Text style={styles.greeting}>Want to help?</Text>
-        <View style={styles.button}>
-          <TouchableOpacity onPress={() => navigate('Amount')}>
-            <View style={styles.button}>
-              <Text style={styles.buttonText}>Donate now!</Text>
-            </View>
-          </TouchableOpacity>
+        <View style={styles.subcontainer1}>
+          <Text style={styles.greeting}>Want to help?</Text>
+          <View>
+            <TouchableOpacity  onPress={() => navigate('Amount')}>
+              <View>
+                <Text style={styles.buttonText}> Donate now!</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.subcontainer2}>
+          <Text style={styles.footer}>Powered By:
+            <Text style={styles.garage}> REALITY GARAGE
+            </Text>
+          </Text>
         </View>
       </View>
     );
@@ -36,15 +44,33 @@ const styles = StyleSheet.create({
     backgroundColor: colors.darkgray,
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  subcontainer1: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   greeting:{
-    fontSize: 30,
+    fontSize: 40,
     fontFamily: fonts.MontserratThin,
     color: colors.white,
-  },
-  button:{
     margin: 20,
-    backgroundColor: colors.yellow,
+  },
+  buttonText: {
+    fontSize: 20,
+    fontFamily: fonts.MontserratThin,
+    color: colors.yellow,
+    borderStyle: 'solid',
+    borderWidth: 2,
+    borderColor: colors.yellow,
+    padding: 12,
     borderRadius: 25,
+  },
+  footer: {
+    fontSize: 15,
+    fontFamily: fonts.MontserratThin,
+    color: colors.white,
+    paddingBottom: 10,
   }
 });
