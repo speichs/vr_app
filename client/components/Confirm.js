29375745d8b32f30814a43b8f192452c69b7338c
Sean Eichenberger <sean.eich@gmail.com>
2017-07-27 17:56:31 -0600

    updated to fetch

--- client/components/Confirm.js
+++ client/components/Confirm.js
@@ -15,15 +15,39 @@ export default class Confirm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      organization: 'Wildlife Protection Solutions'
+      organization: 'Wildlife Protection Solutions',
+      donation: ''
     };
   }
+
+  componentDidMount(){
+     let that = this
+      async function subscribe(path) {
+      const response = await fetch(path, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        }
+      });
+      setTimeout(function() {
+        subscribe("https://reality-garage-server.herokuapp.com/poll");
+      }, 3000);
+      return await response.json().then(function(data){
+        console.log(data)
+        that.setState({donation:data.price});
+      })
+    }//end of subscribe function
+    var data = subscribe("https://reality-garage-server.herokuapp.com/poll");
+  }
+
+
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
         <View style={styles.container3}>
-          <Text style={titleFont}>Your Donation: ${this.props.navigation.state.params.donation}</Text>
+          <Text style={titleFont}>Your Donation: ${this.state.donation}</Text>
           <View style={styles.marginFix}>
             <Text style={titleFont}>To: {this.state.organization}</Text>
           </View>
@@ -34,7 +58,7 @@ export default class Confirm extends React.Component {
             </TouchableOpacity>
             </View>
             <View style={styles.buttonMargin}>
-            <TouchableOpacity  onPress={() => navigate('Card',{donation: this.props.navigation.state.params.donation})}>
+            <TouchableOpacity  onPress={() => navigate('Card',{donation: this.state.donation})}>
               <Text style={button}>Pay Now</Text>
             </TouchableOpacity>
             </View>
